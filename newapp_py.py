6ef2732f8b8c9ae72b31957432e9fdf90e4913cc
ImKshitij09 <75945379+ImKshitij09@users.noreply.github.com>
2024-11-27 17:33:31 +0100

    Update newapp_py.py

--- newapp_py.py
+++ newapp_py.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1FhdBugkcZf7HJh5yg73UlH96wEcqNv3E
 """
 
-!pip install streamlit
 import joblib
 import streamlit as st
 import pandas as pd
